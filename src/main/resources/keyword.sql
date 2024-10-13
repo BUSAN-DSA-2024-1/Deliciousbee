508f83b8e15cfbcd8653f40ac54611516ff134da
dapa0905 <kookangmo0905@gmail.com>
2024-10-13 16:57:18 +0900

    KooKangmo_lastDance
    finalDance

--- src/main/resources/keyword.sql
+++ src/main/resources/keyword.sql
@@ -1,4 +1,18 @@
 INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('VISIT_PURPOSE', '데이트');
 INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('VISIT_PURPOSE', '가족 식사');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('VISIT_PURPOSE', '친구 모임');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('VISIT_PURPOSE', '비즈니스 미팅');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('VISIT_PURPOSE', '혼자서');
+
+
 INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('ATMOSPHERE', '조용한');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('ATMOSPHERE', '활기찬');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('ATMOSPHERE', '아늑한');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('ATMOSPHERE', '세련된');
+INSERT INTO key_word (keywordCategory, keyword_Name) VALUES ('ATMOSPHERE', '편안한');
+
 INSERT INTO Key_word (keywordCategory, keyword_Name) VALUES ('FACILITY', '야외 좌석');
+INSERT INTO Key_word (keywordCategory, keyword_Name) VALUES ('FACILITY', '주차 공간');
+INSERT INTO Key_word (keywordCategory, keyword_Name) VALUES ('FACILITY', '무선 인터넷');
+INSERT INTO Key_word (keywordCategory, keyword_Name) VALUES ('FACILITY', '어린이 놀이 공간');
+INSERT INTO Key_word (keywordCategory, keyword_Name) VALUES ('FACILITY', '장애인 배려 시설');
